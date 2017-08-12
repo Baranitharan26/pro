edb77d5bda57b9a6af88f91729a2d1e0bc4fa984
Baranitharan26 <baranitharan45@gmail.com>
2017-08-12 12:49:28 +0530

    Create login.php

---
+++ .gitignore/login.php
@@ -0,0 +1,8 @@
+
+
+<form action="login.php" method="post">
+  Username: <input type="text" name="username"><br>
+  Password: <input type="password" name="password"><br>
+  <input type="submit" value="Submit">
+</form>
+
