52f83204599e31e844d831542a4b71d9037879b1
Baranitharan26 <baranitharan45@gmail.com>
2017-08-12 12:48:52 +0530

    Update and rename .gitignore to index.php

---
+++ .gitignore/index.php
@@ -0,0 +1,241 @@
+
+<p>
+    R.Baranitharan
+    <img src="barani.jpg" align="right"
+        width="150 "
+        height="150"
+       
+      
+</p>
+<p>
+    BE,Computer Science Engineering,
+</p>
+<p>
+    Contcat_no:9788289850,
+</p>
+<p>
+    Mail_id:baranitharan45@gmail.com.
+</p>
+<p>
+    <strong> </strong>
+</p>
+<p>
+    <strong>Address:</strong>
+</p>
+<p>
+    13\36 samar street,
+</p>
+<p>
+    Dhali
+</p>
+<p>
+    Udumalpet-642116
+    <img
+        width="727"
+        height="1"
+        src="file:///C:/Users/ADMINI~1/AppData/Local/Temp/msohtmlclip1/01/clip_image004.jpg"
+    />
+</p>
+<p>
+    <strong>CAREER OBJECTIVE</strong>
+</p>
+<p>
+    To work in a firm with a professional work driven environment where I can
+    utilize and apply my knowledge, skills which would enable me as a fresh
+    graduate to grow while fulfilling organizational goals.
+</p>
+<p>
+    B<strong>ASIC ACADEMIC CREDENTIALS </strong>
+</p>
+<table border="1" cellspacing="0" cellpadding="0" width="704">
+    <tbody>
+        <tr>
+            <td width="168" valign="top">
+                <p>
+                    <strong>Qualification</strong>
+                </p>
+            </td>
+            <td width="273" valign="top">
+                <p align="center">
+                    <strong>Board/University</strong>
+                </p>
+            </td>
+            <td width="83" valign="top">
+                <p align="center">
+                    <strong>Year</strong>
+                </p>
+            </td>
+            <td width="179" valign="top">
+                <p align="center">
+                    <strong>Percentage</strong>
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="168" valign="top">
+                <p align="center">
+                    BE (Computer Science Engineering)
+                </p>
+            </td>
+            <td width="273" valign="top">
+                <p align="center">
+                    Akshaya College of Engineering and Technology
+                </p>
+            </td>
+            <td width="83" valign="top">
+                <p align="center">
+                    2014-2018
+                </p>
+            </td>
+            <td width="179" valign="top">
+                <p align="center">
+                    5.99/10
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="168" valign="top">
+                <p align="center">
+                    Intermediate
+                </p>
+            </td>
+            <td width="273" valign="top">
+                <p>
+                    State board
+                </p>
+                <p>
+                    R.M.P.T.C
+                </p>
+            </td>
+            <td width="83" valign="top">
+                <p align="center">
+                    2014
+                </p>
+            </td>
+            <td width="179" valign="top">
+                <p align="center">
+                    72%
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td width="168" valign="top">
+                <p align="center">
+                    High School
+                </p>
+            </td>
+            <td width="273" valign="top">
+                <p>
+                    State board
+                </p>
+                <p>
+                    S.K.P.H.S.S
+                </p>
+            </td>
+            <td width="83" valign="top">
+                <p align="center">
+                    2012
+                </p>
+            </td>
+            <td width="179" valign="top">
+                <p align="center">
+                    62.2%
+                </p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<p>
+    <strong><u>Technical Skills</u></strong>
+    <u> </u>
+</p>
+<p>
+    ? <strong>Programming Languages:</strong> C, JAVA ,PYTHON
+</p>
+<p>
+    ? <strong>Design languages: </strong>HTML,CSS,AJAX
+</p>
+<p>
+    ? <strong>Operating Systems:</strong> Linux , Windows XP, Windows 7,
+    Windows 8.1
+</p>
+<p>
+    ? <strong>Databases:</strong> MySQL, DBMS
+</p>
+<p>
+    <strong><u>CO-/EXTRA –CURRICULAR ACTIVITIE</u></strong>
+    <strong><u>S</u></strong>
+    <u></u>
+</p>
+<p>
+? Participated in    <strong>Hybrid android web application development</strong>
+</p>
+<p>
+    ? <strong>Participated in Robotics </strong>
+</p>
+<p>
+    <strong>INTERPERSONAL SKILL</strong>
+</p>
+<p>
+    ? Ability to rapidly build relationship and set up trust.
+</p>
+<p>
+    ? Confident and Determined
+</p>
+<p>
+    ? Ability to cope up with different situations.
+</p>
+<p>
+    <strong>PERSONAL DETAILS</strong>
+</p>
+<p>
+    ? <strong>Father’s Name :- MrT.Rathinasamy</strong>
+</p>
+<p>
+? <strong> Permanent Address :- </strong>    <strong><u>16\36 samar street,</u></strong>
+</p>
+<p>
+    Dhali,
+</p>
+<p>
+    Udumlapet-642116
+</p>
+<p>
+    ? <strong>Date of Birth :- </strong>30 July 1997<strong> </strong>
+</p>
+<p>
+    ? <strong>Language Known :- </strong>English,Tamil<strong> </strong>
+</p>
+<p>
+    ? <strong>Marital Status :- </strong>Single
+</p>
+<p>
+    ? <strong>Nationality/Religion :- </strong>Indian / hindu
+</p>
+<p>
+    ? <strong>Interest &amp; Hobbies :- </strong>Playing cricket and
+    volleyball,watching movies and bike racing
+</p>
+<p>
+    <strong>DECLARATION </strong>
+</p>
+<p>
+    I do hereby declare that the above information is true to the best of my
+    knowledge<strong>.</strong>
+</p>
+<p>
+    <strong> Thanking you,</strong>
+</p>
+<p>
+    <strong>Place: Yours Truly, </strong>
+</p>
+<p>
+    <strong>Date: </strong>
+</p>
+<p>
+    <strong> </strong>
+</p>
+<p>
+    <strong> (R.BARANITHARAN) </strong>
+</p>
+<a href="login.php?logout">Logout</a>
